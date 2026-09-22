de2472585556acb90ba696683efc3e890fd6bac7
manish <ngtgmgptpj@gmail.com>
2026-09-22 16:50:14 +0530

    new code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,10 +11,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
-        orderService order = context.getBean(orderService.class);
-        order.placeOrder();
-        User user = context.getBean(User.class);
-        System.out.println(user.getName());
+    ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+    orderService order = context.getBean(orderService.class);
     }
 }
--- src/main/java/org/example/payment/paymentMethod.java
+++ src/main/java/org/example/payment/paymentMethod.java
@@ -1,5 +1,8 @@
 package org.example.payment;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+//@Autowired
 public interface paymentMethod {
     public void pay();
 }
