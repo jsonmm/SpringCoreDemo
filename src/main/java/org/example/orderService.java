5ea17f055a129c65061a4a1690f47259d2fc58ba
manish <ngtgmgptpj@gmail.com>
2026-09-22 12:13:26 +0530

    setter method se dependency injection

--- src/main/java/org/example/orderService.java
+++ src/main/java/org/example/orderService.java
@@ -10,11 +10,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class orderService {
     public paymentMethod pay ;
+//    @Autowired
+//    public orderService(@Qualifier("cod") paymentMethod pay)
+//    {
+//        this.pay = pay;
+//    }
     @Autowired
-    public orderService(@Qualifier("cod") paymentMethod pay)
-    {
+    public void setPay(@Qualifier("cod") paymentMethod pay) {
         this.pay = pay;
     }
+
     public void placeOrder()
     {
         pay.pay();
